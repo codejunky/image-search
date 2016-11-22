2e860ac9869472b92ebb161df39b52bfe3a656ee
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-23 00:45:24 +0100

    Create functionality to allow the user to search for images using the imgur API and to see his most recent searches

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const request = require('request');
+const moment = require('moment');
+const path = require('path');
 
+const History = require('./models/History');
 
 var app = express();
 
+mongoose.connect('mongodb://fcc:Password1!@ds035826.mlab.com:35826/image-search');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
 app.get('/api/imagesearch/:query', (req, res) => {
-  res.json({
-    message: 'Hello world!'
+  let query = req.params.query;
+  let page = req.query.offset;
+  let time = moment(new Date).unix();
+  let url = `https://api.imgur.com/3/gallery/search/time/${page}?q_all=${query}`;
+
+  // Track the user searches
+  const entry = new History({term: query, when: time});
+  entry.save((err, hist) => {
+    console.log(hist)
+    console.log('User search saved to db');
   });
+
+  // Query imgur API for the term supplied by the user
+  const requestOptions = {
+    url: url,
+    headers: {
+      'Authorization': 'Client-ID 1ebf988fc8c852b'
+    }
+  }
+
+  request(requestOptions, (err, response, body) => {
+    res.json({
+      data: JSON.parse(body).data
+    })
+  });
+});
+
+
+app.get('/api/latest/imagesearch', (req, res) => {
+  History.find({})
+         .sort({when: -1})
+         .limit(10)
+         .exec((err, entries) => {
+           res.json({
+             history: entries
+           })
+         });
 });
 
 app.listen(process.env.PORT || 3000, () => {
